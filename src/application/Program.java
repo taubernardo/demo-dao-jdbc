428257c8dac45c61e3fc7e91d0419a858aea81e9
tbernardo <tbptffc@gmail.com>
2023-07-30 15:47:27 -0300

    Department entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,60 +1,12 @@
 package application;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import db.DB;
+
+import model.entities.Department;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Connection conn = null;
-        PreparedStatement st = null;
-        ResultSet rs = null;
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
         
-
-        try{ 
-            conn = DB.getConnection();
-            st = conn.prepareStatement(
-                "INSERT INTO seller "
-                + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                + "VALUES "
-                + "(?, ?, ?, ?, ?)",
-                java.sql.PreparedStatement.RETURN_GENERATED_KEYS);
-
-            
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1995").getTime()));
-            st.setDouble(4, 3000.00);
-            st.setInt(5, 4);
-
-            int rowsAffected = st.executeUpdate();
-
-            if (rowsAffected > 0) {
-                rs = st.getGeneratedKeys();
-                while(rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Done! Id = " + id);
-                }
-            } else {
-                System.out.println("No rows affected.");
-            }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        } 
-        catch (ParseException e) {
-            e.printStackTrace();
-        }
-        finally {
-            DB.closeStatement(st);
-            DB.closeResultSet(rs);
-            DB.closeConnection();
-        }
-
-}
 
 }
